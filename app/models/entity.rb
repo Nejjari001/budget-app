7f394bf5287f38373a7d6f80cbf83783efab2f58
AbdoNJ <mr.nejjari.abdelhafid@gmail.com>
2023-03-16 16:33:33 +0100

    Update models

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,7 +1,10 @@
 class Entity < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_and_belongs_to_many :groups
 
-  validates :name, presence: true
-  validates :amount, presence: true
+  has_many :entities_groups, dependent: :destroy
+  has_many :groups, through: :entities_groups, dependent: :destroy
+
+  # Valudation
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
